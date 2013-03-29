568538ff35886abcde07a7b2cb2cd461ced3939c
John Hume <duelin.markers@gmail.com>
2013-03-29 11:17:21 -0500

    Add scheduler fns

--- test/expectations/jreg_expectations.clj
+++ test/expectations/jreg_expectations.clj
@@ -1,8 +1,10 @@
 (set! *warn-on-reflection* true)
+
 (ns expectations.jreg-expectations
-  (:import (org.jetlang.core Callback SynchronousDisposingExecutor)
+  (:import (java.util.concurrent TimeUnit)
+           (org.jetlang.core Callback Scheduler SynchronousDisposingExecutor)
            (org.jetlang.channels MemoryChannel))
-  (:use expectations jreg))
+  (:use erajure.core expectations jreg))
 
 (expect Callback (->callback identity))
 (expect 1 (let [a (atom 0)]
@@ -20,3 +22,17 @@
 (expect "ran" (let [a (atom nil)]
                 (execute (SynchronousDisposingExecutor.) #(reset! a "ran"))
                 @a))
+
+(set! *warn-on-reflection* false) ; because type-hints on interaction aren't working.
+
+(expect-let [scheduler (mock Scheduler)]
+            (interaction (.schedule scheduler a-fn 23 TimeUnit/MILLISECONDS))
+            (schedule scheduler a-fn 23))
+
+(expect-let [scheduler (mock Scheduler)]
+            (interaction (.scheduleAtFixedRate scheduler a-fn 5 10 TimeUnit/MILLISECONDS))
+            (schedule-at-fixed-rate scheduler a-fn 5 10))
+
+(expect-let [scheduler (mock Scheduler)]
+            (interaction (.scheduleWithFixedDelay scheduler a-fn 5 10 TimeUnit/MILLISECONDS))
+            (schedule-with-fixed-delay scheduler a-fn 5 10))
