ecae8e223c5493639fd063422ffeba49bc9dedee
John Hume <duelin.markers@gmail.com>
2013-03-29 11:17:21 -0500

    Add scheduler fns

--- src/clojure/jreg.clj
+++ src/clojure/jreg.clj
@@ -1,7 +1,9 @@
 (ns jreg
-  (:import (java.util.concurrent Executor)
+  (:import (java.util.concurrent Executor
+                                 TimeUnit)
            (org.jetlang.core Callback
-                             Disposable)
+                             Disposable
+                             Scheduler)
            (org.jetlang.channels Publisher
                                  Subscriber)))
 
@@ -18,3 +20,23 @@
 
 (defn execute [^Executor executor runnable]
   (.execute executor runnable))
+
+(defn schedule
+  (^Disposable [^Scheduler scheduler command delay time-unit]
+               (.schedule scheduler command delay time-unit))
+  (^Disposable [scheduler command delay-millis]
+               (schedule scheduler command delay-millis TimeUnit/MILLISECONDS)))
+
+(defn schedule-at-fixed-rate
+  (^Disposable [^Scheduler scheduler command initial-delay period time-unit]
+               (.scheduleAtFixedRate scheduler command initial-delay period time-unit))
+  (^Disposable [scheduler command initial-delay-millis period-millis]
+               (schedule-at-fixed-rate scheduler command initial-delay-millis period-millis
+                                       TimeUnit/MILLISECONDS)))
+
+(defn schedule-with-fixed-delay
+  (^Disposable [^Scheduler scheduler command initial-delay delay time-unit]
+               (.scheduleWithFixedDelay scheduler command initial-delay delay time-unit))
+  (^Disposable [scheduler command initial-delay-millis delay-millis]
+               (schedule-with-fixed-delay scheduler command initial-delay-millis delay-millis
+                 TimeUnit/MILLISECONDS)))
