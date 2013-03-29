1e2fc16fd055caa8990a5e6a1ea6cd0b0c228c74
John Hume <duelin.markers@gmail.com>
2013-03-29 14:05:52 -0500

    Add ->last-subscriber

--- test/expectations/jreg_expectations.clj
+++ test/expectations/jreg_expectations.clj
@@ -1,9 +1,10 @@
 (set! *warn-on-reflection* true)
 
 (ns expectations.jreg-expectations
-  (:import (java.util.concurrent TimeUnit)
+  (:import (java.util.concurrent CountDownLatch TimeUnit)
            (org.jetlang.core Callback Disposable Scheduler SynchronousDisposingExecutor)
-           (org.jetlang.channels MemoryChannel))
+           (org.jetlang.channels MemoryChannel)
+           (org.jetlang.fibers ThreadFiber))
   (:use erajure.core expectations jreg))
 
 (expect 1 (let [a (atom 0)]
@@ -28,13 +29,31 @@
 
 (expect ["good message"]
         (let [chan (MemoryChannel.)
-              a (atom [])]
+              received (atom [])]
           (subscribe chan (->channel-subscriber (SynchronousDisposingExecutor.)
-                                                #(swap! a conj %)
+                                                #(swap! received conj %)
                                                 #(re-find #"good" %)))
           (publish chan "good message")
           (publish chan "bad message")
-          @a))
+          @received))
+
+(expect ["first message" "third message"]
+        (let [chan (MemoryChannel.)
+              consuming-fiber (ThreadFiber.)
+              received (atom [])
+              latch (CountDownLatch. 2)]
+          (.start consuming-fiber)
+          (subscribe chan (->last-subscriber 4 TimeUnit/MILLISECONDS
+                                             consuming-fiber
+                                             (fn [m]
+                                               (swap! received conj m)
+                                               (.countDown latch))))
+          (publish chan "first message")
+          (Thread/sleep 6)
+          (publish chan "second message")
+          (publish chan "third message")
+          (.await latch 5 TimeUnit/MILLISECONDS)
+          @received))
 
 (expect "ran" (let [a (atom nil)]
                 (execute (SynchronousDisposingExecutor.) #(reset! a "ran"))
