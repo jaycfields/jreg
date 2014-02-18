b401760721ead69febc83ff1f68d6386751a5f6d
Jay Fields <jay@jayfields.com>
2014-02-18 15:34:16 -0500

    convert the rest

--- test/expectations/jreg_expectations.clj
+++ test/expectations/jreg_expectations.clj
@@ -55,107 +55,102 @@
   (doto (mock Scheduler)
     (schedule-with-fixed-delay a-fn1 (interval 5 :secs) (interval 10 :secs))))
 
-(comment
-  (defn core-fns-have-return-type-hints []
-    (let [cb (->callback nil)] (.onMessage cb nil))
-    (let [f (->filter nil)] (.passes f nil))
-    (let [d (schedule nil nil nil)] (.dispose d))
-    (let [d (schedule-at-fixed-rate nil nil nil nil)] (.dispose d))
-    (let [d (schedule-with-fixed-delay nil nil nil nil)] (.dispose d)))
+(defn core-fns-have-return-type-hints []
+  (let [cb (->callback nil)] (.onMessage cb nil))
+  (let [f (->filter nil)] (.passes f nil))
+  (let [d (schedule nil nil nil)] (.dispose d))
+  (let [d (schedule-at-fixed-rate nil nil nil nil)] (.dispose d))
+  (let [d (schedule-with-fixed-delay nil nil nil nil)] (.dispose d)))
 
-  ;; Channels
+;; Channels
 
-  (expect 1
+(expect 1
+  (let [chan (MemoryChannel.)
+        executor (SynchronousDisposingExecutor.)
+        a (atom 0)]
+    (subscribe chan executor #(swap! % inc))
+    (publish chan a)
+    @a))
+
+(expect ["good message"]
+  (let [chan (MemoryChannel.)
+        received (atom [])]
+    (subscribe chan (->channel-subscriber #(re-find #"good" %)
+                                          (SynchronousDisposingExecutor.)
+                                          #(swap! received conj %)))
+    (publish chan "good message")
+    (publish chan "bad message")
+    @received))
+
+(expect ["first message" "third message"]
+  (from-each [[flush-interval time-unit] {1 TimeUnit/MILLISECONDS
+                                          (interval 1 :millis) TimeUnit/MILLISECONDS}]
     (let [chan (MemoryChannel.)
-          executor (SynchronousDisposingExecutor.)
-          a (atom 0)]
-      (subscribe chan executor #(swap! % inc))
-      (publish chan a)
-      @a))
+          consuming-fiber (ThreadFiber.)
+          received (atom [])
+          latch (CountDownLatch. 2)]
+      (.start consuming-fiber)
+      (subscribe chan (->last-subscriber flush-interval
+                                         consuming-fiber
+                                         (fn [m]
+                                           (swap! received conj m)
+                                           (.countDown latch))))
+      (publish chan "first message")
+      (Thread/sleep (+ 2 (.toMillis time-unit 1)))
+      (publish chan "second message")
+      (publish chan "third message")
+      (.await latch 2 time-unit)
+      @received)))
 
-  (expect ["good message"]
+(expect [["first message"] ["second message" "third message"]]
+  (from-each [[flush-interval time-unit] {1 TimeUnit/MILLISECONDS
+                                          (interval 1 :millis) TimeUnit/MILLISECONDS}]
     (let [chan (MemoryChannel.)
-          received (atom [])]
-      (subscribe chan (->channel-subscriber #(re-find #"good" %)
-                                            (SynchronousDisposingExecutor.)
-                                            #(swap! received conj %)))
-      (publish chan "good message")
-      (publish chan "bad message")
-      @received))
+          consuming-fiber (ThreadFiber.)
+          received (atom [])
+          latch (CountDownLatch. 2)]
+      (.start consuming-fiber)
+      (subscribe chan (->batch-subscriber flush-interval
+                                          consuming-fiber
+                                          (fn [m]
+                                            (swap! received conj m)
+                                            (.countDown latch))))
+      (publish chan "first message")
+      (Thread/sleep (+ 2 (.toMillis time-unit 1)))
+      (publish chan "second message")
+      (publish chan "third message")
+      (.await latch 2 time-unit)
+      @received)))
 
-  (given [flush-interval time-unit]
-    (expect ["first message" "third message"]
-      (let [chan (MemoryChannel.)
-            consuming-fiber (ThreadFiber.)
-            received (atom [])
-            latch (CountDownLatch. 2)]
-        (.start consuming-fiber)
-        (subscribe chan (->last-subscriber flush-interval
-                                           consuming-fiber
-                                           (fn [m]
-                                             (swap! received conj m)
-                                             (.countDown latch))))
-        (publish chan "first message")
-        (Thread/sleep (+ 2 (.toMillis time-unit 1)))
-        (publish chan "second message")
-        (publish chan "third message")
-        (.await latch 2 time-unit)
-        @received))
-    1 TimeUnit/MILLISECONDS
-    (interval 1 :millis) TimeUnit/MILLISECONDS)
+(expect [{"A" "A 1"}
+         {"A" "A 3" "B" "B 1"}]
+  (from-each [[flush-interval time-unit] {1 TimeUnit/MILLISECONDS
+                                          (interval 1 :millis) TimeUnit/MILLISECONDS}]
+    (let [chan (MemoryChannel.)
+          consuming-fiber (ThreadFiber.)
+          received (atom [])
+          latch (CountDownLatch. 2)]
+      (.start consuming-fiber)
+      (subscribe chan (->keyed-batch-subscriber #(.substring ^String % 0 1)
+                                                flush-interval
+                                                consuming-fiber
+                                                (fn [m]
+                                                  (swap! received conj m)
+                                                  (.countDown latch))))
+      (publish chan "A 1")
+      (Thread/sleep (+ 2 (.toMillis time-unit 1)))
+      (publish chan "A 2")
+      (publish chan "A 3")
+      (publish chan "B 1")
+      (.await latch 2 time-unit)
+      @received)))
 
-  (given [flush-interval time-unit]
-    (expect [["first message"] ["second message" "third message"]]
-      (let [chan (MemoryChannel.)
-            consuming-fiber (ThreadFiber.)
-            received (atom [])
-            latch (CountDownLatch. 2)]
-        (.start consuming-fiber)
-        (subscribe chan (->batch-subscriber flush-interval
-                                            consuming-fiber
-                                            (fn [m]
-                                              (swap! received conj m)
-                                              (.countDown latch))))
-        (publish chan "first message")
-        (Thread/sleep (+ 2 (.toMillis time-unit 1)))
-        (publish chan "second message")
-        (publish chan "third message")
-        (.await latch 2 time-unit)
-        @received))
-    1 TimeUnit/MILLISECONDS
-    (interval 1 :millis) TimeUnit/MILLISECONDS)
-
-  (given [flush-interval time-unit]
-    (expect [{"A" "A 1"}
-             {"A" "A 3" "B" "B 1"}]
-      (let [chan (MemoryChannel.)
-            consuming-fiber (ThreadFiber.)
-            received (atom [])
-            latch (CountDownLatch. 2)]
-        (.start consuming-fiber)
-        (subscribe chan (->keyed-batch-subscriber #(.substring ^String % 0 1)
-                                                  flush-interval
-                                                  consuming-fiber
-                                                  (fn [m]
-                                                    (swap! received conj m)
-                                                    (.countDown latch))))
-        (publish chan "A 1")
-        (Thread/sleep (+ 2 (.toMillis time-unit 1)))
-        (publish chan "A 2")
-        (publish chan "A 3")
-        (publish chan "B 1")
-        (.await latch 2 time-unit)
-        @received))
-    1 TimeUnit/MILLISECONDS
-    (interval 1 :millis) TimeUnit/MILLISECONDS)
-
-  (defn channel-fns-have-return-type-hints []
-    (let [d (subscribe nil nil)] (.dispose d))
-    (let [d (subscribe nil nil nil)] (.dispose d))
-    (let [s (->channel-subscriber nil nil)] (.getQueue s))
-    (let [s (->channel-subscriber nil nil nil)] (.getQueue s))
-    (let [s (->last-subscriber nil nil nil)] (.getQueue s))
-    (let [s (->last-subscriber nil nil nil nil)] (.getQueue s))
-    (let [s (->batch-subscriber nil nil nil)] (.getQueue s))
-    (let [s (->batch-subscriber nil nil nil nil)] (.getQueue s)))
-)
+(defn channel-fns-have-return-type-hints []
+  (let [d (subscribe nil nil)] (.dispose d))
+  (let [d (subscribe nil nil nil)] (.dispose d))
+  (let [s (->channel-subscriber nil nil)] (.getQueue s))
+  (let [s (->channel-subscriber nil nil nil)] (.getQueue s))
+  (let [s (->last-subscriber nil nil nil)] (.getQueue s))
+  (let [s (->last-subscriber nil nil nil nil)] (.getQueue s))
+  (let [s (->batch-subscriber nil nil nil)] (.getQueue s))
+  (let [s (->batch-subscriber nil nil nil nil)] (.getQueue s)))
