80e6aac13a157084610e59238ca967d43ae55c10
Jay Fields <jay@jayfields.com>
2014-12-01 09:54:50 -0500

    update project

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies [[expectations "2.0.4"]
                                   [erajure "0.0.4"]
                                   [org.jetlang/jetlang "0.2.10"]]}}
